e14133e3bb8a00b4696bcdb7f0d0401592c8fdf4
Matthew Wiesner <wiesner@jhu.edu>
2023-03-21 17:46:16 -0400

    Trying sphinx docs again

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,6 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx_rtd_theme",
-    "sphinx_autodoc_typehints",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
