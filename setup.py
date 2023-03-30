9bb2538d488cd1f627025b798e4a1b0879c27ef7
Matthew Wiesner <wiesner@jhu.edu>
2023-03-30 14:46:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setuptools.setup(
     license='Apache License 2.0',
 
     python_requires='>=3.8',
-    packages=setuptools.find_packages(),
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
