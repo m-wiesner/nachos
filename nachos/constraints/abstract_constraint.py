1b46af177cf1178f9d406509eaccfce4e7e637e8
Matthew Wiesner <wiesner@jhu.edu>
2023-03-31 12:53:57 -0400

    Improves docstring of abstract_constraint.py

--- nachos/constraints/abstract_constraint.py
+++ nachos/constraints/abstract_constraint.py
@@ -3,6 +3,20 @@ from abc import ABC, abstractmethod
 
 
 class AbstractConstraint(ABC):
+    '''
+        Summary:
+            Defines the abstract base class for all constraints used for
+            splitting datasets. By default, values for constraints can be
+            multivalued. This can occur for instance when there are two speakers
+            speaking in a single recording with different genders. Or if there
+            are multiple durations of speech specified for each speaker
+            in a recording. 
+            
+            Some constraint methods can work directly on the
+            sets, while other require a specific reduction method. This
+            reduction method is the only parameter passed to the base class.
+            Other constraints may require specifying additional parameters.
+    '''
     @classmethod
     def build(cls, conf: dict):
         return cls(reduction=conf['constraint_reduction'])
@@ -24,6 +38,16 @@ class AbstractConstraint(ABC):
                 When a constraint is set valued, how do we reduce the
                 constraint to be a single float? The reduction method
                 specified determines this.
+
+                Values for reduction can be:
+                    - 'mean'
+                    - 'sum'
+                    - 'min'
+                    - 'max'
+
+                They are specified in the constraint_reduction field of the 
+                yaml configuration file passed to the build method of this
+                class. 
             
             :param s: The set of floats to be reduced
             :type s: set
