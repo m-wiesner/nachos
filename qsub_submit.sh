825232dfdb9017df776c7425e255f0ec08436e5e
Matthew Wiesner <wiesner@jhu.edu>
2023-05-26 12:20:36 -0400

    Updates qsub_submit.sh to automatically find the best random seed and plot the constraint scores after completing a batch of jobs

--- qsub_submit.sh
+++ qsub_submit.sh
@@ -21,10 +21,19 @@ mkdir -p ${odir}
 
 for s in `seq 0 ${nj}`; do
   mkdir -p ${odir}/seed${s}
-  if [[ ! -z $partition && ! -z $split ]]; then
-    qsub -l h_rt=20:00:00 -V -o ${odir}/seed${s}/split.log -e ${odir}/seed${s}/split.err -N split.${s} -cwd ./run.sh --partition-and-split "${partition}:${split}" --use-components ${use_components} ${s} ${odir}/seed${s} ${config} ${tsv}
-  else
-    qsub -l h_rt=20:00:00 -V -o ${odir}/seed${s}/split.log -e ${odir}/seed${s}/split.err -N split.${s} -cwd ./run.sh --use-components ${use_components} ${s} ${odir}/seed${s} ${config} ${tsv}
-  fi
+  (
+    if [[ ! -z $partition && ! -z $split ]]; then
+      qsub -l h_rt=20:00:00 -V -o ${odir}/seed${s}/split.log -e ${odir}/seed${s}/split.err -N split.${s} -cwd -sync y ./run.sh --partition-and-split "${partition}:${split}" --use-components ${use_components} ${s} ${odir}/seed${s} ${config} ${tsv}
+    else
+      qsub -l h_rt=20:00:00 -V -o ${odir}/seed${s}/split.log -e ${odir}/seed${s}/split.err -N split.${s} -cwd -sync y ./run.sh --use-components ${use_components} ${s} ${odir}/seed${s} ${config} ${tsv}
+    fi
+  ) &
 done
+wait
+
+echo "Finished splitting data. Computing and plotting some statistics ..."
+# Find the best seed and plot the evolution of the different constraints
+# during the course of the split search.
+python scripts/get_best_seed.py --top-k 1 ${odir} > ${odir}/best_seed
+python scripts/plot_constraints.py ${odir} ${odir}/constraint_scores.png
 
